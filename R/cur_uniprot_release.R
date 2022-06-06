75a86be1b029fef11812a149c19fef0831f9b77a
Charlotte Dawson <csdaw@outlook.com>
2022-06-06 15:25:23 +0100

    Use httr2 to check UniProt release
    More robust than readLines.

--- R/cur_uniprot_release.R
+++ R/cur_uniprot_release.R
@@ -14,9 +14,12 @@
 #' }
 #'
 cur_uniprot_release <- function() {
-  # First line of current release notes text has release number
-  release <- readLines("https://ftp.uniprot.org/pub/databases/uniprot/current_release/relnotes.txt", n = 1L)
+  # Construct GET request for human actin
+  req <- httr2::request("https://rest.uniprot.org/uniprotkb/P60709")
 
-  # Get rid of extraneous letters and whitespace
-  gsub("[^0-9_]", "", release)
+  # Perform GET request
+  resp <- httr2::req_perform(req)
+
+  # Extract release number from HTTP response header
+  resp %>% httr2::resp_header("x-release-number")
 }
