84ae41a1803642f05a8c7630d57a911cc0d58b63
Charlotte Dawson <csdaw@outlook.com>
2022-12-26 21:03:32 +1100

    Clean up `uniprot_post()`

--- dev/refactor/test_uniprot_post.R
+++ dev/refactor/test_uniprot_post.R
@@ -1,37 +1,36 @@
-library(here)
 library(magrittr)
+library(httr2)
 
-rest_url <- "https://rest.uniprot.org/idmapping"
+rest_url <- "https://rest.uniprot.org/idmapping/run"
 
-input <- readLines(here("dev/map-id-test-input.txt"))[-10:-50]
-
+input <- c("Q92945", "Q96AE4")
 
 ## Original
 
-post_req <- httr2::request(rest_url) %>%
-  httr2::req_user_agent("uniprotREST https://github.com/csdaw/uniprotREST") %>%
-  httr2::req_url_path_append("run") %>%
-  httr2::req_body_form(
-    from = "UniProtKB_AC-ID",
-    to = "UniProtKB",
-    ids = paste(input, collapse = ",")
-  ) %>%
-  httr2::req_retry(max_tries = 5) %>%
-  httr2::req_throttle(rate = 1 / 1) # limit: 1 request every 1 second
-
-httr2::req_dry_run(post_req)
+# post_req <- httr2::request(rest_url) %>%
+#   httr2::req_user_agent("uniprotREST https://github.com/csdaw/uniprotREST") %>%
+#   httr2::req_body_form(
+#     from = "UniProtKB_AC-ID",
+#     to = "UniProtKB",
+#     ids = paste(input, collapse = ",")
+#   ) %>%
+#   httr2::req_retry(max_tries = 5) %>%
+#   httr2::req_throttle(rate = 1 / 1) # limit: 1 request every 1 second
+#
+# httr2::req_dry_run(post_req)
 
 ## New
 
 post_resp <- uniprot_post(
-  url = paste(rest_url, "run", sep = "/"),
+  url = rest_url,
   from = "UniProtKB_AC-ID",
   to = "UniProtKB",
   ids = paste(input, collapse = ","),
-  dry_run = FALSE
+  verbosity = 1,
+  dry_run = TRUE
 )
 
-get_jobid(post_resp)
+get_job_id(post_resp)
 
 
 
--- dev/refactor/uniprot_post.R
+++ dev/refactor/uniprot_post.R
@@ -11,9 +11,15 @@
 uniprot_post <- function(url,
                          ...,
                          max_tries = 5,
-                         rate = 1,
+                         rate = 1 / 1,
                          verbosity = NULL,
                          dry_run = FALSE) {
+  assert_url(url)
+  assert_integerish(max_tries, max.len = 1)
+  assert_numeric(rate, lower = 0)
+  if (!is.null(verbosity)) assert_integerish(verbosity, lower = 0, upper = 3, max.len = 1)
+  assert_logical(dry_run)
+
   post_req <- httr2::request(url) %>%
     httr2::req_method("POST") %>%
     httr2::req_user_agent("uniprotREST https://github.com/csdaw/uniprotREST") %>%
@@ -21,7 +27,7 @@ uniprot_post <- function(url,
     httr2::req_throttle(rate = rate) %>%
     httr2::req_body_form(...)
 
-  if (dry_run) return(httr2::req_dry_run(post_req))
+  if (dry_run) return(httr2::req_dry_run(post_req, quiet = ifelse(is.null(verbosity), TRUE, FALSE)))
   else httr2::req_perform(post_req, verbosity = verbosity)
 }
 
