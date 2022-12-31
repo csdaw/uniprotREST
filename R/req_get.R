c03e7523dd92bc7d539d5d0257ee56508e963091
Charlotte Dawson <csdaw@outlook.com>
2022-12-31 11:03:52 +1100

    Rewrite uniprot request fun
    Makes it flexible to different request
    methods. Avoids code duplication.

--- R/req_get.R
+++ R/req_get.R
@@ -1,19 +1,27 @@
 #' @export
-req_get <- function(url,
-                    ...,
-                    max_tries = 5,
-                    rate = 1 / 1) {
+uniprot_request <- function(url,
+                            method = "GET",
+                            ...,
+                            max_tries = 5,
+                            rate = 1 / 1) {
   ## Argument checks
   # ensure request isn't malformed
   assert_url(url)
+  assert_choice(method, c("GET", "POST", "HEAD"))
   assert_integerish(max_tries, max.len = 1)
   assert_numeric(rate, lower = 0, max.len = 1)
 
-  ## Return request object
-  httr2::request(url) %>%
-    httr2::req_method("GET") %>%
+  ## Construct base request object
+  req <- httr2::request(url) %>%
     httr2::req_user_agent("uniprotREST https://github.com/csdaw/uniprotREST") %>%
     httr2::req_retry(max_tries = max_tries) %>%
-    httr2::req_throttle(rate = rate) %>%
-    httr2::req_url_query(...)
+    httr2::req_throttle(rate = rate)
+
+  ## Modify HTTP request based on method
+  switch(
+    method,
+    GET = httr2::req_url_query(req, ...),
+    POST = httr2::req_body_form(req, ...),
+    HEAD = httr2::req_options(req, nobody = TRUE)
+  )
 }
